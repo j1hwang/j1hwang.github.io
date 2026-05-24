a4db549cb8d3de901f99f5a75d9294c33d31ee3c
Jaewon Hwang <jaewon.hwang@cre.ma>
2026-05-24 13:00:22 +0900

    Add: 글 하단 같은 폴더 내 이웃 글 목록 (NearbyPosts) 컴포넌트

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.Sunlit()],
+  afterBody: [Component.NearbyPosts(), Component.Sunlit()],
   footer: Component.Footer(),
 }
 
